ac1381fdc801ac282cd3b70d66a870e2bff83865
Alexandra Ioan <alexandra@eitr.co>
2023-04-07 14:07:47 +0200

    dockerizing

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require("axios")
 const app = express();
-const cors = require("cors");
 
 app.use(bodyParser.json());
-app.use(cors())
+
+const events = [];
 
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:8000/events', event).catch((err) => {
         console.log(err.message);
@@ -16,11 +18,14 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:8001/events', event).catch((err) => {
         console.log(err.message);
     });
-    // axios.post('http://localhost:4002/events', event).catch((err) => {
-    //     console.log(err.message);
-    // });
-    res.send({ status: 'OK' });
+    axios.post('http://localhost:8003/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    res.send({status: 'OK'});
+});
 
+app.get('/events', (req, res) => {
+    res.send(events);
 })
 
 app.listen(8005, () => {
